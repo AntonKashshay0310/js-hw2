ef049e401fa6eac80c598bef317b9d206664b6fd
AntonKashshay0310 <antosha.kashshay@gmail.com>
2025-01-30 10:13:17 +0200

    add

---
+++ js/script1.js
@@ -0,0 +1,13 @@
+// 1
+const celsium = 50;
+const farenheit = (celsium * 1.8 + 32);
+console.log(`Conversion ${celsium}C = ${farenheit}F`);
+// 2
+const days = 31;
+const hours = days * 24;
+const minutes = hours * 60;
+console.log(`У цьому місяці: Днів:${days},Годин:${hours},Хвилин:${minutes} `);
+// 3
+const health = 100;
+const energy = 100;
+console.log(`Здоровя гравця: ${health - 15}, Енергія гравця: ${energy - 50}`);
