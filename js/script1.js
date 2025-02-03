a6d8d7fe7a98aa0792fd08284e3052e104d6b7c1
AntonKashshay0310 <antosha.kashshay@gmail.com>
2025-02-03 17:58:29 +0200

    add

--- js/script1.js
+++ js/script1.js
@@ -11,3 +11,22 @@ console.log(`У цьому місяці: Днів:${days},Годин:${hours},Х
 const health = 100;
 const energy = 100;
 console.log(`Здоровя гравця: ${health - 15}, Енергія гравця: ${energy - 50}`);
+// 4
+const products = 2718;
+console.log(`Продукти без знижки ${products} Продукти зі знижкою ${products / 10}`);
+// 5
+const notanumber = 27.18;
+console.log(`Число${notanumber} Заокруглене число ${Math.floor(notanumber)}`);
+// 6
+const numberKoma = 18.18;
+console.log(`Число${numberKoma} Десяткове число ${parseFloat(numberKoma)}`);
+// 7
+const numberWithKoma = "18.18";
+console.log(`Число${numberWithKoma} Ціле число ${parseInt(numberWithKoma)}`);
+// 8
+const korin = 196;
+console.log(`Число${korin} Корінь цього числа ${Math.sqrt(korin)}`);
+// 9
+const justNumber = 18;
+const stringNumber = "27";
+console.log(`З рядка в число, результат: ${parseInt(stringNumber)} З числа в рядок, результат:${toString(justNumber)}`);
